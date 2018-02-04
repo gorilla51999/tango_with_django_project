43e3f947d56cfc588ce09284ba921ba926dc6cc0
Chen Gao <2275321g@student.gla.ac.uk>
2018-02-04 23:59:30 +0000

    update2

--- rango/views.py
+++ rango/views.py
@@ -153,6 +153,23 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
+def visitor_cookie_handler(request, response):
+
+	visits = int(request.COOKIES.get('visits', '1'))
+	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+	                                         '%Y-%m-%d %H:%M:%S')
+
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+
+		response.set_cookie('last_visit', str(datetime.now()))
+	else:
+		visits = 1
+
+		response.set_cookie('last_visit', last_visit_cookie)
+
+	response.set_cookie('visits', visits)
 
 
 def get_server_side_cookie(request, cookie, default_val=None):
